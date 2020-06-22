a8738dbb5e37b207808847e8442b896623f1efef
AuroraLMQ <49789986+AuroraLMQ@users.noreply.github.com>
2020-06-22 20:50:19 +0800

    商品页面左侧标签栏

---
+++ product_left.php
@@ -0,0 +1,62 @@
+
+<div class="col-md-3 prdt-right">
+    <div class="w_sidebar">
+        <section  class="sky-form">
+            <h1>分类</h1>
+            <div class="row1 scroll-pane">
+                <div class="col col-4">
+                    <label>全部产品</label>
+                </div>
+                <div class="col col-4">
+                    </p><label>珠宝</label></p>
+                    </p><label>皮包</label></p>
+                    </p><label>手表</label></p>
+                    </p><label>玩具</label></p>
+                </div>
+            </div>
+        </section>
+        <section  class="sky-form">
+            <h2>品牌</h2>
+            <div class="row1 row2 scroll-pane">
+                <div class="col col-4">
+                    </p><label>所有品牌</label></p>
+                </div>
+                <div class="col col-4">
+                    </p><label>Armani/阿玛尼</label></p>
+                    </p><label>Cartier/卡地亚</label></p>
+                    </p><label>Gucci/古奇</label></p>
+                    </p><label>杜嘉班纳</label></p>
+                    </p><label>施华洛世奇</label></p>
+                    </p><label>Pandora/潘多拉</label></p>
+                    </p><label>乐高</label></p>
+                    </p><label>barbie系列</label></p>
+                </div>
+            </div>
+
+            <section class="sky-form">
+                <h4>颜色</h4>
+                <ul class="w_nav2">
+                    <li><a class="color1" href="#"></a></li>
+                    <li><a class="color2" href="#"></a></li>
+                    <li><a class="color3" href="#"></a></li>
+                    <li><a class="color4" href="#"></a></li>
+                    <li><a class="color5" href="#"></a></li>
+                    <li><a class="color6" href="#"></a></li>
+                    <li><a class="color7" href="#"></a></li>
+                    <li><a class="color8" href="#"></a></li>
+                    <li><a class="color9" href="#"></a></li>
+                    <li><a class="color10" href="#"></a></li>
+                    <li><a class="color12" href="#"></a></li>
+                    <li><a class="color13" href="#"></a></li>
+                    <li><a class="color14" href="#"></a></li>
+                    <li><a class="color15" href="#"></a></li>
+                    <li><a class="color5" href="#"></a></li>
+                    <li><a class="color6" href="#"></a></li>
+                    <li><a class="color7" href="#"></a></li>
+                    <li><a class="color8" href="#"></a></li>
+                    <li><a class="color9" href="#"></a></li>
+                    <li><a class="color10" href="#"></a></li>
+                </ul>
+            </section>
+    </div>
+</div> 
