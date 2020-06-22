cfbd88d0c9f5ac62f89ffcb04d029ff47d075a00
shuentang <48541494+shuentang@users.noreply.github.com>
2020-06-22 20:00:51 +0800

    Revert "admin 页面"

--- getadmin.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if(isset($_SESSION['id'])){
-		$sql99 = "SELECT * from admin WHERE id='{$_SESSION['id']}'";
-		$mysqli_result99 = $mysqli->query($sql99);
-		$name = $mysqli_result99->fetch_array(MYSQLI_ASSOC);
-	}
-?>
